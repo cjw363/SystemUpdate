cd7f228f04c2c4acccdd488c3e02da465eaa709b
陈家炜 <771613512@qq.com>
2018-01-23 09:50:26 +0800

    通过android studio第三次提交代码，测试

--- app/src/main/java/tecsun/cjw/systemupdate/MainActivity.java
+++ app/src/main/java/tecsun/cjw/systemupdate/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    System.out.println("主线程"+Thread.currentThread().getId());
+//    System.out.println("主线程"+Thread.currentThread().getId());
     OkHttpUtil.getInstance().doHttp("http://cpzx.e-tecsun.com:8037/update/TA/update_cjw.xml");
   }
 }
